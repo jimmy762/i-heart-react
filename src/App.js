2186aaa9cb65b0df0fbc8d3b5d287dcdbfa66a2a
Sam <samuel.bouk@gmail.com>
2022-04-06 18:39:52 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import HeartsList from './components/HeartsList';
+import Header from './components/Header';
+
+const message = 'cool cud';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <HeartsList />;
+    </>
+  )
+}
+
+export default App;
